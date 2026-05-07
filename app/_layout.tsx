9257e21bb27af568474f022f04e9b30a0f7f9868
Carmen Alexandre Salido <carmen.salido006@gmail.com>
2026-05-07 09:21:37 -0300

    feat: tela de login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack 
+  screenOptions={{
+      headerShown: false,
+      title: "Hydra",
+      //não funciona
+      headerBackIcon: {
+      type: 'image',
+      source: require('C:/Users/CARMEN/MAUÁ/CIC/projeto-integrador-3/Hydra_v2/src/images/logo-sao-camilo.jpg'),
+    }
+    }}
+    >
+    
+    <Stack.Screen
+      name="HYdra"/>;
+      </Stack>
 }
