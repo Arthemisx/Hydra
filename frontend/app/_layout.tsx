ecbcbbf3417fc2de999691021a2edcc0253d4505
Bianca-BS2 <bborgessantana@hotmail.com>
2026-06-06 13:55:27 -0300

    Adição do VLibras

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
-
 import { ProvedorFeedback } from "@/componentes/ProvedorFeedback";
+import VLibras from "@/componentes/VLibras";
 
 export default function RootLayout() {
   return (
     <ProvedorFeedback>
+      <VLibras forceOnload />
       <Stack screenOptions={{ headerShown: false }} />
     </ProvedorFeedback>
   );
